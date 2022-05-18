5f4d8d958efc14e8e50424ce5c47655cb9c8d5c7
Ivan-Shaml <stu1901681077@uni-plovdiv.bg>
2022-05-18 15:48:41 +0300

    Not working version!!  Changed button colors. TODO: Implement the Categories activity wth Grid Layout

---
+++ app/src/main/java/dev/ivanshamliev/fishermansstore/CatalogActivity.java
@@ -0,0 +1,14 @@
+package dev.ivanshamliev.fishermansstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+    }
+}
